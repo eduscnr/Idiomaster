b760e1fbcd20662acce640a812ad607e71844803
scnredu <scnredu@gmail.com>
2024-01-30 22:35:30 +0100

    Implementacion de un minijuego

--- app/src/main/java/com/example/idiomaster/ui/minijuegos/TraducePalabras.java
+++ app/src/main/java/com/example/idiomaster/ui/minijuegos/TraducePalabras.java
@@ -1,26 +1,16 @@
 package com.example.idiomaster.ui.minijuegos;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.idiomaster.R;
 
-public class TraducePalabras extends Fragment {
-
-
-    public TraducePalabras() {
-        // Required empty public constructor
-    }
+public class TraducePalabras extends AppCompatActivity {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_traduce_palabras, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traduce_palabras);
     }
 }
