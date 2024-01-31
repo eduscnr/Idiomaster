fd8794be29fe8a6e15130a76700eb27cb861afc5
scnredu <scnredu@gmail.com>
2024-01-31 10:37:01 +0100

    Implementacion de registrar

---
+++ app/src/main/java/com/example/idiomaster/ui/Registro.java
@@ -0,0 +1,16 @@
+package com.example.idiomaster.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.idiomaster.R;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
