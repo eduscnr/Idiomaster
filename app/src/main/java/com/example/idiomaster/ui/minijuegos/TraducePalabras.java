3bffe5f9c6d57d0e5b07c4371070e63b9fed1dca
MADAFAKASn1 <marcosandresmoldovanclase@gmail.com>
2024-02-03 19:40:10 +0100

    Traduce Palabras Validacion

--- app/src/main/java/com/example/idiomaster/ui/minijuegos/TraducePalabras.java
+++ app/src/main/java/com/example/idiomaster/ui/minijuegos/TraducePalabras.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.gridlayout.widget.GridLayout;
 
+import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.idiomaster.MainActivity;
@@ -22,25 +24,42 @@ public class TraducePalabras extends AppCompatActivity implements View.OnClickLi
     private ActivityTraducePalabrasBinding binding;
     private List<String> opciones;
     private GridLayout gridLayout;
+    private Button btnvalidar;
+    private int indiceActual = 0;
+    private String respuestPulsada;
+    private TextView tvCorrectoInCorrecto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityTraducePalabrasBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        opciones = new ArrayList<>();
-        Random random = new Random();
-        binding.palabraTraducir.setText(MainActivity.getNivelSeleccionado().getPalabras().get(0));
+
+        tvCorrectoInCorrecto = binding.textviewCorrectoIncorrecto;
         gridLayout = binding.gridJugar;
+
+        opciones = new ArrayList<>();
+        binding.palabraTraducir.setText(MainActivity.getNivelSeleccionado().getPalabras().get(indiceActual));
+
         for (int i = 0; i <= 10; i++) {
             opciones.add(MainActivity.getNivelSeleccionado().getPalabras().get(i));
         }
         aniadeHijos(10);
         recorrer();
+
+        btnvalidar = binding.validar;
+        btnvalidar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btnvalidar.isEnabled()) {
+                    validarRespuesta();
+                }
+            }
+        });
     }
 
     public void aniadeHijos(int k) {
-        gridLayout.setColumnCount(1);
-        gridLayout.setRowCount(3);
+        gridLayout.setColumnCount(3);
+        gridLayout.setRowCount(1);
         Button b;
         for (int i = 0; i <= k; i++) {
             b = new Button(binding.getRoot().getContext());
@@ -68,17 +87,30 @@ public class TraducePalabras extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onClick(View view) {
         Button button = (Button) view;
-
+        respuestPulsada = button.getText().toString();
         if (view.getClass().getSimpleName().equals("Button")){
             Button b = (Button) view;
             recorrer();
-            b.setBackgroundColor(ContextCompat.getColor(this, R.color.white));
+            b.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_200));
         }
+    }
 
-        if(button.getText().toString().equalsIgnoreCase(binding.palabraTraducir.getText().toString())){
-            Toast.makeText(this, "Correcto", Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(this, "Incorrecto", Toast.LENGTH_SHORT).show();
+    public void validarRespuesta(){
+        if (respuestPulsada.equalsIgnoreCase(binding.palabraTraducir.getText().toString())) {
+            //Toast.makeText(this, "Correcto", Toast.LENGTH_SHORT).show();
+            tvCorrectoInCorrecto.setText("CORRECTO");
+            tvCorrectoInCorrecto.setTextColor(ContextCompat.getColor(this, R.color.teal_700));
+            indiceActual++;
+            if (indiceActual < MainActivity.getNivelSeleccionado().getPalabras().size()) {
+                binding.palabraTraducir.setText(MainActivity.getNivelSeleccionado().getPalabras().get(indiceActual));
+            } else {
+               // Toast.makeText(this, "¡Juego completado!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        } else {
+            //Toast.makeText(this, "Incorrecto", Toast.LENGTH_SHORT).show();
+            tvCorrectoInCorrecto.setText("INCORRECTO");
+            tvCorrectoInCorrecto.setTextColor(ContextCompat.getColor(this, R.color.purple_200));
         }
     }
 }
