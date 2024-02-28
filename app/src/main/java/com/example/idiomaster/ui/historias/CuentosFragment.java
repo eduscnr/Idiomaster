354b630872e9c96885899f95be9f78bf31500255
Manuel <martinezdorol@gmail.com>
2024-02-28 11:01:52 +0100

    cambios en el cuentos

--- app/src/main/java/com/example/idiomaster/ui/historias/CuentosFragment.java
+++ app/src/main/java/com/example/idiomaster/ui/historias/CuentosFragment.java
@@ -1,4 +1,4 @@
-package com.example.idiomaster.ui.historias;
+package com.example.idiomaster.ui.cuentos;
 
 import android.graphics.Paint;
 import android.os.Bundle;
@@ -8,33 +8,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.idiomaster.R;
 import com.example.idiomaster.adaptadores.AdaptadorCuentos;
-import com.example.idiomaster.adaptadores.AdaptadorMundo;
 import com.example.idiomaster.databinding.FragmentStoriesBinding;
 import com.example.idiomaster.iniciar.IniciarSesion;
 import com.example.idiomaster.modelo.Cuento;
-import com.example.idiomaster.modelo.Mundo;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
-public class StoriesFragment extends Fragment {
+public class CuentosFragment extends Fragment {
 
     private FragmentStoriesBinding binding;
     private RecyclerView rvCuentos;
@@ -46,18 +39,20 @@ public class StoriesFragment extends Fragment {
     private AdaptadorCuentos adaptadorCuentos;
     private AdaptadorCuentos.listener listener;
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_stories, container, false);
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentStoriesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         rvCuentos = binding.rvCuentos;
         facil =binding.easyButton;
+        facil.setTag("facil");
         intermedio =binding.intermediateButton;
+        intermedio.setTag("intermedio");
         dificil =binding.advancedButton;
+        dificil.setTag("dificil");
 
         rdgDificultad =binding.difficultyRadioGroup;
 
@@ -66,18 +61,15 @@ public class StoriesFragment extends Fragment {
             intermedio.setPaintFlags(0);
             dificil.setPaintFlags(0);
 
-            RadioButton selectedButton =  view.findViewById((checkedId));
-            String dificultadSeleccionada = (String) selectedButton.getText();
+            RadioButton selectedButton =  root.findViewById((checkedId));
+            String dificultadSeleccionada = (String) selectedButton.getTag();
             Log.d("Boton seleccionado:", (String) selectedButton.getText());
             if (selectedButton != null) {
                 Log.d("", "se va a pintar el boton " + selectedButton.getText());
                 selectedButton.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
             }
-            intermedio.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
-            //TODO OBTENER CUENTOS SELECCIONADOS
-            cuentosSeleccionados = obtenerCuentos(dificultadSeleccionada);
-            adaptadorCuentos = new AdaptadorCuentos(cuentosSeleccionados,listener);
-            rvCuentos.setAdapter(adaptadorCuentos);
+            ponerCuentos(dificultadSeleccionada);
+
 
         });
         return root;
@@ -89,7 +81,7 @@ public class StoriesFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-    public List<Cuento> obtenerCuentos(String dificultad){
+    public void ponerCuentos(String dificultad){
         List<Cuento>cuentos = new ArrayList<>();
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = firebaseDatabase.getReference("historias/"+ IniciarSesion.getInicioSesionUsuario().getIdioma()+"/" + dificultad);
@@ -100,16 +92,13 @@ public class StoriesFragment extends Fragment {
                     Cuento cuento = d.getValue(Cuento.class);
                     cuentos.add(cuento);
                 }
-                /*
-                adaptadorMundo = new AdaptadorMundo(mundos, listenerMundo);
-                recyclerViewMundos.setAdapter(adaptadorMundo);*/
+                adaptadorCuentos = new AdaptadorCuentos(cuentos,listener);
+                rvCuentos.setAdapter(adaptadorCuentos);
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
-        return cuentos;
     }
 }
